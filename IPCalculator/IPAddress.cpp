173764b22f48a571dacc76f1105abcc47d270966
Ovidiu PODARIU <ocpodariu@yahoo.com>
2015-03-09 14:03:18 +0200

    Overload left-shift operator to print ip using quad-dotted notation

--- IPCalculator/IPAddress.cpp
+++ IPCalculator/IPAddress.cpp
@@ -1,3 +1,12 @@
 #include "IPAddress.h"
 
-IPAddress::IPAddress(unsigned char a, unsigned char b, unsigned char c, unsigned char d) : m_a(a), m_b(b), m_c(c), m_d(d) {}
+IPAddress::IPAddress (unsigned char a, unsigned char b, unsigned char c, unsigned char d) : m_a(a), m_b(b), m_c(c), m_d(d) {}
+
+std::ostream& operator<< (std::ostream& os, const IPAddress& addr) {
+    os << (int) addr.m_a << '.'
+       << (int) addr.m_b << '.'
+       << (int) addr.m_c << '.'
+       << (int) addr.m_d;
+
+    return os;
+}
--- IPCalculator/IPAddress.h
+++ IPCalculator/IPAddress.h
@@ -1,8 +1,12 @@
+#include <iostream>
+
 // An IPv4 address has the following form:
 // "A.B.C.D", where A, B, C and D are bytes
 class IPAddress {
  public:
-     IPAddress(unsigned char a = 0, unsigned char b = 0, unsigned char c = 0, unsigned char d = 0);
+     IPAddress (unsigned char a = 0, unsigned char b = 0, unsigned char c = 0, unsigned char d = 0);
+
+     friend std::ostream& operator<< (std::ostream& os, const IPAddress& addr);
 
  private:
     unsigned char m_a;
