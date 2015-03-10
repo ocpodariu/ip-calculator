4edb16c1422a04285915fe8af989168ca1e76ee2
Ovidiu PODARIU <ocpodariu@yahoo.com>
2015-03-10 23:58:54 +0200

    Overload right- and left-shift operators for easier reading/writing

--- IPCalculator/InternetAddress.cpp
+++ IPCalculator/InternetAddress.cpp
@@ -1,3 +1,17 @@
 #include "InternetAddress.h"
 
 InternetAddress::InternetAddress (IPAddress& ip, IPAddress& mask) : m_address(ip), m_subnet_mask(mask) {}
+
+std::ostream& operator<< (std::ostream& os, const InternetAddress& addr) {
+    os << "IP: " << addr.m_address     << std::endl;
+    os << "SN: " << addr.m_subnet_mask << std::endl;
+
+    return os;
+}
+
+std::istream& operator>> (std::istream& is, InternetAddress& addr) {
+    is >> addr.m_address;
+    is >> addr.m_subnet_mask;
+
+    return is;
+}
--- IPCalculator/InternetAddress.h
+++ IPCalculator/InternetAddress.h
@@ -3,12 +3,17 @@
 
 #include "IPAddress.h"
 
+#include <iostream>
+
 // An Internet address is defined by an IP address and
 // a subnet mask. It can represent a network address,
 // a broadcast address or a host address.
 class InternetAddress {
  public:
     InternetAddress (IPAddress& ip, IPAddress& mask);
+
+    friend std::ostream& operator<< (std::ostream& os, const InternetAddress& addr);
+	friend std::istream& operator>> (std::istream& is, InternetAddress& addr);
 
  private:
     IPAddress m_address;
