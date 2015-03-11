9c7f04e44b82b463a5dc517d6cb3116ad52906b4
Ovidiu PODARIU <ocpodariu@yahoo.com>
2015-03-11 22:41:21 +0200

    Network details must be recalculated after each modification of ip or subnet mask

--- IPCalculator/InternetAddress.cpp
+++ IPCalculator/InternetAddress.cpp
@@ -26,16 +26,24 @@ IPAddress InternetAddress::getIPAddress () const { return m_address; }
 
 IPAddress InternetAddress::getSubnetMask () const { return m_subnet_mask; }
 
-void InternetAddress::setIPAddress (IPAddress& ip) { m_address = ip; }
+void InternetAddress::setIPAddress (IPAddress& ip) {
+    m_address = ip;
+    calculateNB();
+}
 
 void InternetAddress::setIPAddress (unsigned char a, unsigned char b, unsigned char c, unsigned char d) {
     m_address.setAddress(a, b, c, d);
+    calculateNB();
 }
 
-void InternetAddress::setSubnetMask (IPAddress& mask) { m_subnet_mask = mask; }
+void InternetAddress::setSubnetMask (IPAddress& mask) {
+    m_subnet_mask = mask;
+    calculateNB();
+}
 
 void InternetAddress::setSubnetMask (unsigned char a, unsigned char b, unsigned char c, unsigned char d) {
     m_subnet_mask.setAddress(a, b, c, d);
+    calculateNB();
 }
 
 void InternetAddress::calculateNB () {
