38c559318deb1c78a3dac9191936386dd83bb5e0
Ovidiu PODARIU <ocpodariu@yahoo.com>
2015-03-16 22:10:03 +0200

    Add method for network subnetting

--- IPCalculator/Util.cpp
+++ IPCalculator/Util.cpp
@@ -1,5 +1,7 @@
 #include "Util.h"
 
+#include <algorithm>
+
 int Util::identifyAddress(const InternetAddress& addr) {
     if (addr.isNetworkAddress())
         return 0;
@@ -7,4 +9,50 @@ int Util::identifyAddress(const InternetAddress& addr) {
         return 1;
     else
         return 2;
+}
+
+bool compare(int x, int y) {
+    return x > y;
+}
+
+std::vector<InternetAddress> Util::subnetNetwork (InternetAddress network, std::vector<int> hosts) {
+    std::vector<InternetAddress> subnetworks;
+    subnetworks.reserve(hosts.size());
+
+    // Make sure the address is the first available
+    // host IP address of the network
+    network.setIPAddress(network.getNetworkAddress().nextIPAddress());
+
+    // Each subnetwork (i) needs to accommodate (hosts[i] + 2)
+    // IPs (1 for the network address and 1 for the broadcast)
+    for (unsigned int i = 0; i < hosts.size(); i++)
+        hosts[i] += 2;
+
+    // Sort the subnetworks in descending order to ensure
+    // a correct subnetting process
+    std::sort(hosts.begin(), hosts.end(), compare);
+
+    int n;
+    unsigned int mask_32bit;
+    IPAddress subnet_mask;
+    
+    for (unsigned int i = 0 ; i < hosts.size(); i++) {
+        // Find smallest power of 2  >= hosts[i]
+        n = 1;
+        while (hosts[i] > n)
+            n = n << 1;
+
+        // Determine corresponding subnet mask
+        mask_32bit = n - 1;
+        network.setSubnetMask(~mask_32bit);
+
+        // Store this subnetwork
+        subnetworks.push_back(InternetAddress(network.getIPAddress(), network.getSubnetMask()));
+
+        // Set the starting IP address of the next subnetwork
+        // (the first available host IP address, not the network address)
+        network.setIPAddress(network.getBroadcastAddress().nextIPAddress().nextIPAddress());
+    }
+
+    return subnetworks;
 }
--- IPCalculator/Util.h
+++ IPCalculator/Util.h
@@ -3,6 +3,8 @@
 
 #include "InternetAddress.h"
 
+#include <vector>
+
 // Helper class that includes methods for IP network
 // processes such as identifying the type of an address.
 class Util {
@@ -12,6 +14,11 @@ class Util {
     //      (1) broadcast address;
     //      (2) host address.
     static int identifyAddress (const InternetAddress& address);
+    
+    // Subnet a network into multiple smaller subnetworks.
+    // Each subnetwork (i) needs to accommodate hosts[i] hosts.
+    // The resulting vector contains all the subnetworks.
+    static std::vector<InternetAddress> subnetNetwork (InternetAddress network, std::vector<int> hosts);
 };
 
 #endif // _UTIL_H_
